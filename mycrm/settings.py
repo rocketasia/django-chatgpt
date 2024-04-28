cf5dacd2996e7e50aa2bf5a9601cf947c8298e16
rocketasia <74543570+rocketasia@users.noreply.github.com>
2024-04-28 20:22:06 +0800

    css strap

--- mycrm/settings.py
+++ mycrm/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
