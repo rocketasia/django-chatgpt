a5a77210a6e9b292d959527c2d1916e98c42ea3f
rocketasia <74543570+rocketasia@users.noreply.github.com>
2024-05-12 00:04:13 +0800

    f

--- crm/models.py
+++ crm/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=255)
    
-    phone = models.CharField(max_length=255)
+   
     email = models.EmailField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
