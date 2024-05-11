24f00ed5bb99b19faa91b3d81ed3233973b72fb9
rocketasia <74543570+rocketasia@users.noreply.github.com>
2024-05-11 23:28:30 +0800

    Update models.py

--- crm/models.py
+++ crm/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class Customer(models.Model):
-    firstname = models.CharField(max_length=255)
-    surname = models.CharField(max_length=255)
-    phone = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    
     email = models.EmailField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.firstname
+
+
 
