e68e6bd027dd67aff5eeb8bf6e1ff4782bbd50ef
rocketasia <74543570+rocketasia@users.noreply.github.com>
2024-05-11 21:13:09 +0800

    Update settings.py

--- mycrm/settings.py
+++ mycrm/settings.py
@@ -92,12 +92,22 @@ WSGI_APPLICATION = 'mycrm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'crm/db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'crm/db.sqlite3',
-    }
-}
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'database-2',
+        'USER': 'postgres',
+        'PASSWORD': ',,,,,,,,',
+        'HOST': 'database-2.c3ggwoscgyki.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }}
 
 
 # Password validation
