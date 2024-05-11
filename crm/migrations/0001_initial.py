54c362ee3f9273caf9a47c0ebf7b890223101781
rocketasia <74543570+rocketasia@users.noreply.github.com>
2024-05-12 00:13:13 +0800

    g

--- crm/migrations/0001_initial.py
+++ crm/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
             name='Customer',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255)),
+                ('firstname', models.CharField(max_length=255)),('surname', models.CharField(max_length=255)),('phone', models.CharField(max_length=255)),
                 ('email', models.EmailField(max_length=254, unique=True)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
             ],
