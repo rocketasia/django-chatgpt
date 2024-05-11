d4cfc68456e91abddc4f0c3e1feece9d35556331
rocketasia <74543570+rocketasia@users.noreply.github.com>
2024-05-11 23:34:21 +0800

    Update models.py

--- crm/models.py
+++ crm/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Customer(models.Model):
     name = models.CharField(max_length=255)
+    test = models.CharField(max_length=255)
     
     email = models.EmailField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
