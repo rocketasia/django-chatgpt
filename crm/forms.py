9dceac099851c3776c0f8ce81a88478277146aca
rocketasia <74543570+rocketasia@users.noreply.github.com>
2024-05-11 23:29:41 +0800

    Update forms.py

--- crm/forms.py
+++ crm/forms.py
@@ -4,4 +4,4 @@ from .models import Customer
 class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
-        fields = ['email', 'phone']  # List all the fields you want from the model
+        fields = ['email', 'name']  # List all the fields you want from the model
